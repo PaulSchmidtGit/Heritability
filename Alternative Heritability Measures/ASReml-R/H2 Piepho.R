33d79e7fd0b3feb0d2971e2fe9d34c5268925594
Paul Schmidt <42671379+PaulSchmidtGit@users.noreply.github.com>
2018-08-29 21:52:37 +0200

    Update H2 Piepho.R

--- Alternative Heritability Measures/ASReml-R/H2 Piepho.R
+++ Alternative Heritability Measures/ASReml-R/H2 Piepho.R
@@ -10,11 +10,11 @@ dat <- john.alpha
 ##############
 require(asreml)
 # Genotype as random effect
-g_ran <- asreml(fixed = yield ~       rep, 
+g.ran <- asreml(fixed = yield ~       rep, 
                 random=       ~ gen + rep:block, 
                 data=dat)
 # Genotype as fixed effect
-g_fix <- asreml(fixed = yield ~ gen + rep,
+g.fix <- asreml(fixed = yield ~ gen + rep,
                 random=       ~       rep:block, 
                 data=dat)
 
@@ -22,19 +22,19 @@ g_fix <- asreml(fixed = yield ~ gen + rep,
 # Handle model estimates #
 ##########################
 # Genetic variance component
-gen_var <- summary(g_ran)$varcomp['gen!gen.var','component']
-gen_var #0.142902
+gen.var <- summary(g.ran)$varcomp['gen!gen.var','component']
+gen.var #0.142902
 
 # Mean variance of a difference of two GBLUEs
-vdBLUE.mat <- predict(g_fix, classify="gen", sed=TRUE)$pred$sed^2 # obtain squared s.e.d. matrix 
+vdBLUE.mat <- predict(g.fix, classify="gen", sed=TRUE)$pred$sed^2 # obtain squared s.e.d. matrix 
 vdBLUE.avg <- mean(vdBLUE.mat[upper.tri(vdBLUE.mat, diag=FALSE)]) # take mean of upper triangle
 vdBLUE.avg #0.07010875
 
 #############
 # H2 Piepho #
 #############
-H2_p <- gen_var/(gen_var + vdBLUE.avg/2)
-H2_p #0.803017
+H2.p <- gen.var/(gen.var + vdBLUE.avg/2)
+H2.p #0.803017
 
 
 

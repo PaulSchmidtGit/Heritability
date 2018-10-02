4f6fe1785605c6283dd4f7486fd051ffd91d2a00
Paul Schmidt <31928213+SchmidtPaul@users.noreply.github.com>
2018-10-02 16:31:45 +0200

    fixed typo

--- Alternative Heritability Measures/ASReml-R/H2 Cullis.R
+++ Alternative Heritability Measures/ASReml-R/H2 Cullis.R
@@ -18,16 +18,16 @@ g.ran <- asreml(fixed = yield ~       rep,
 # Handle model estimates #
 ##########################
 # Genetic variance component
-gen_var <- summary(g.ran)$varcomp['gen!gen.var','component']
-gen_var #0.142902
+gen.var <- summary(g.ran)$varcomp['gen!gen.var','component']
+gen.var #0.142902
 
 # Mean variance of a difference of two GBLUPs
-vdBLUP.mat <- predict(g_ran, classify="gen", only="gen", sed=TRUE)$pred$sed^2 # obtain squared s.e.d. matrix 
+vdBLUP.mat <- predict(g.ran, classify="gen", only="gen", sed=TRUE)$pred$sed^2 # obtain squared s.e.d. matrix 
 vdBLUP.avg <- mean(vdBLUP.mat[upper.tri(vdBLUP.mat, diag=FALSE)]) # take mean of upper triangle
 vdBLUP.avg #0.05455038
 
 #############
 # H2 Cullis #
 #############
-H2Cullis <- 1 - (vdBLUP.avg / 2 / gen_var)
+H2Cullis <- 1 - (vdBLUP.avg / 2 / gen.var)
 H2Cullis #0.8091336

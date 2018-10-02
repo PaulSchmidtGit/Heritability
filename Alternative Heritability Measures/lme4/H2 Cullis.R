855822d713f95361259ac0a654b3d899e3ceb2ba
Paul Schmidt <31928213+SchmidtPaul@users.noreply.github.com>
2018-10-02 19:03:24 +0200

    Minor Update

--- Alternative Heritability Measures/lme4/H2 Cullis.R
+++ Alternative Heritability Measures/lme4/H2 Cullis.R
@@ -1,4 +1,5 @@
 rm(list = ls())
+library(data.table)
 #######################
 # Import example data #
 #######################
@@ -8,34 +9,34 @@ dat <- john.alpha
 library(lme4)
 library(lmerTest)
 
-mod <- lmer(data    = dat,
+g.ran <- lmer(data    = dat,
             formula = yield ~ rep + (1|gen) + (1|rep:block))
 
 # manually reconstruct mixed model equation for this specific example
 # to obtain var-cov-matrix for BLUPs of gen effect.
-vc   <- as.data.table(VarCorr(mod)) # extract estimated variance components (vc)
+vc   <- as.data.table(VarCorr(g.ran)) # extract estimated variance components (vc)
 
 # R = varcov-matrix for error term
-n    <- length(summary(mod)$residuals) # numer of observations
+n    <- length(summary(g.ran)$residuals) # numer of observations
 vc.e <- vc[grp=="Residual", vcov]      # error vc
 R    <- diag(n)*vc.e                   # R matrix = I * vc.e
 
 # G = varcov-matrx for all random effects
   # varcov-matrix for genotypic effect
-  n.g  <- summary(mod)$ngrps["gen"]    # number of genotypes
+  n.g  <- summary(g.ran)$ngrps["gen"]    # number of genotypes
   vc.g <- vc[grp=="gen", vcov]         # genotypic vc
   G.g  <- diag(n.g)*vc.g               # gen part of G matrix = I * vc.g
   
   # varcov-matrix for incomplete block effect
-  n.b  <- summary(mod)$ngrps["rep:block"] # number of incomplete blocks
+  n.b  <- summary(g.ran)$ngrps["rep:block"] # number of incomplete blocks
   vc.b <- vc[grp=="rep:block", vcov]      # incomplete block vc
   G.b  <- diag(n.b)*vc.b                  # incomplete block part of G matrix = I * vc.b
   
 G <- bdiag(G.g, G.b) # G is blockdiagonal with G.g and G.b
 
 # Design Matrices
-X <- as.matrix(getME(mod, "X")) # Design matrix fixed effects
-Z <- as.matrix(getME(mod, "Z")) # Design matrix random effects
+X <- as.matrix(getME(g.ran, "X")) # Design matrix fixed effects
+Z <- as.matrix(getME(g.ran, "Z")) # Design matrix random effects
 
 # Mixed Model Equation (HENDERSON 1986; SEARLE et al. 2006)
 C11 <- t(X) %*% solve(R) %*% X
